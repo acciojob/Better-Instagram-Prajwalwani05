a6884d4d9672cae7c7721983c9fe7a1068f53881
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-25 16:54:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const divs = document.querySelectorAll('div');
+
+divs.forEach(div => {
+  div.addEventListener('dragstart', dragStart);
+  div.addEventListener('dragover', dragOver);
+  div.addEventListener('drop', drop);
+});
